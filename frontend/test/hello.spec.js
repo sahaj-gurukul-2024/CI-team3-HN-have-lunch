1904e5d2c3b00964b3ba76fba6aa0e91678b066b
Dheeraj Bisht <dheerajbisht876@gmail.com>
2024-02-22 16:15:14 +0530

    Add Hello World frontend

--- frontend/test/hello.spec.js
+++ frontend/test/hello.spec.js
@@ -2,19 +2,25 @@ const chrome = require('selenium-webdriver/chrome');
 const {By, Builder} = require('selenium-webdriver');
 const assert = require("assert");
 
-(async function firstTest() {
+describe("Get Index", () => {
   let driver;
-  
-  try {
-    driver = await new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().addArguments('--headless')).build();
+  before(async () => {
+    driver = new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().addArguments('--headless')).build();
+  });
+
+  after(async () => {
+    await driver.quit();
+  })
+
+  it("should have correct title", async () => {
     await driver.get('http://127.0.0.1:5500/src/index.html');
-  
     let title = await driver.getTitle();
     assert.equal("Office Lunch Tracker", title);
-  
-  } catch (e) {
-    console.log(e)
-  } finally {
-    await driver.quit();
-  }
-}())
+  });
+
+  it("should have correct body", async () => {
+    const title = await driver.findElement(By.id('title'));
+    const titleText = await title.getText();
+    assert.equal("Hello world", titleText);
+  });
+});
