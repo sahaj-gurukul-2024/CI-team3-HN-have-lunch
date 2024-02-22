02417dc6a73fc6e412c55d2a4a8b612842de13ae
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2024-02-22 15:10:12 +0530

    Create HealthCheckController in backend

---
+++ backend/src/main/kotlin/com/lunch/controllers/HealthCheckController.kt
@@ -0,0 +1,13 @@
+package com.lunch.controllers
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.annotation.Produces
+
+@Controller("/health-check")
+class HealthCheckController {
+    @Get("/")
+    @Produces("text/plain")
+    fun check() = HttpResponse.ok("Hello World!")
+}
