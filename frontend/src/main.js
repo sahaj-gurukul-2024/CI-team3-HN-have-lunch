c409709ae283882603ebda48dbc505a4c995cd7e
Ragul Shanmugarajan <raguls@Raguls-MacBook-Pro.local>
2024-02-22 16:45:05 +0530

    add login form

---
+++ frontend/src/main.js
@@ -0,0 +1,23 @@
+const env = import.meta.env
+
+async function login(event) {
+    event.preventDefault()
+    const id = document.getElementById("id").value
+    const name = document.getElementById("name").value
+
+    const loginData = {
+        id,
+        name
+    }
+    const response = await fetch(`http://3.88.227.174:8080/uat`,{
+        body : JSON.stringify(loginData),
+        method : "POST",
+        headers : {
+            "Content-Type" : "application/json"
+        }
+    })
+
+    const data = await response.json()
+
+    console.log(loginData)
+}
--- frontend/test/login.spec.js
+++ frontend/test/login.spec.js
@@ -1,11 +1,12 @@
 const chrome = require('selenium-webdriver/chrome');
 const {By, Builder} = require('selenium-webdriver');
-const assert = require("assert");
+const {assert} = require("assert");
 
 describe("Get Index", () => {
   let driver;
   before(async () => {
     driver = new Builder().forBrowser('chrome').setChromeOptions(new chrome.Options().addArguments('--headless')).build();
+    await driver.get(`http://3.88.227.174/uat`);
   });
 
   after(async () => {
@@ -13,14 +14,13 @@ describe("Get Index", () => {
   })
 
   it("should have correct title", async () => {
-    await driver.get('http://127.0.0.1:5500/src/index.html');
     let title = await driver.getTitle();
     assert.equal("Office Lunch Tracker", title);
   });
 
   it("should have correct body", async () => {
-    const title = await driver.findElement(By.id('title'));
+    const title = await driver.findElement(By.tagName('h1'));
     const titleText = await title.getText();
-    assert.equal("Hello world", titleText);
+    assert.equal("Welcome", titleText);
   });
 });
