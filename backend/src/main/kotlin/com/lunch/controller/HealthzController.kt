8819e9d080a22f1c5f5c9a38fd8548c53411161b
Dheeraj Bisht <dheerajbisht876@gmail.com>
2024-02-23 21:36:21 +0530

    add healthz endpoint

--- backend/src/main/kotlin/com/lunch/controller/HealthzController.kt
+++ backend/src/main/kotlin/com/lunch/controller/HealthzController.kt
@@ -1,13 +1,14 @@
 package com.lunch.controller
 
 import io.micronaut.http.HttpResponse
+import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Produces
 
-@Controller("/health-check")
-class HealthCheckController {
-    @Get("/")
-    @Produces("text/plain")
-    fun check() = HttpResponse.ok("Hello World!")
+@Controller("/healthz")
+class HealthzController {
+    @Get
+    @Produces(MediaType.TEXT_PLAIN)
+    fun check() = HttpResponse.ok("ok")
 }
---
+++ backend/src/test/kotlin/com/lunch/HealtzServiceTest.kt
@@ -0,0 +1,20 @@
+package com.lunch
+
+import io.micronaut.http.HttpRequest
+import io.micronaut.http.client.HttpClient
+import io.micronaut.http.client.annotation.Client
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+
+@MicronautTest
+class HealtzServiceTest(@Client("/") val client: HttpClient) {
+    @Test
+    fun testHello() {
+        val request: HttpRequest<Any> = HttpRequest.GET("/healthz")
+        val body = client.toBlocking().retrieve(request)
+        assertNotNull(body)
+        assertEquals("ok", body)
+    }
+}
