753d5a3ab1edd1040f606d85c449eec2f4dc5cec
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2024-02-23 16:11:24 +0530

    Fix Base Url path in frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite"
 
+const Mode = process.env.APP_BASE_HREF || '';
+
 export default defineConfig({
+    base: "${Mode}",
     experimental: {
         renderBuiltUrl(filename, { hostType }) {
             return { relative: true }
