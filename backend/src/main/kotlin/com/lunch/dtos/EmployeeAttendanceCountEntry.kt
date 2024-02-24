dc6447009556339e4a4e2958a4771c19d7d79ff9
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2024-02-24 17:32:49 +0530

    Remove Micronaut Backend 💀

--- backend/src/main/kotlin/com/lunch/dtos/EmployeeAttendanceCountEntry.kt
+++
@@ -1,7 +0,0 @@
-package com.lunch.dtos
-
-import io.micronaut.serde.annotation.Serdeable
-import java.time.LocalDate
-
-@Serdeable
-data class EmployeeAttendanceCountEntry(val date: LocalDate, val count: Int)
