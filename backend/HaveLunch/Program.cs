ea8a9469ae560fbd0e71b49689e3ea49f14c3d78
Sathiyaraman M <Sathiyaraman2003@gmail.com>
2024-02-24 18:20:23 +0530

    Streamline Program.cs in Main project

--- backend/HaveLunch/Program.cs
+++ backend/HaveLunch/Program.cs
@@ -3,11 +3,20 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors();
+
 var connectionString = builder.Configuration.GetConnectionString("PostgresConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
 
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors();
+app.UseRouting();
+
+app.MapGet("/health-checkg", () => "Hello World!");
+
+app.MapControllers();
 
 app.Run();
