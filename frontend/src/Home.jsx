ffcbff6c66a9ae6508834e33bac47b372a7ff8db
Dheeraj Bisht <dheerajbisht876@gmail.com>
2024-02-24 12:37:44 +0530

    improve ui

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -50,67 +50,67 @@ function Home() {
   }, []);
 
   return (
-    <Container className="d-flex flex-column justify-content-center align-items-center vh-100 w-50">
-      <Toast onClose={() => setShow(false)} show={show} className="position-absolute top-0 end-0" animation autohide delay={1000}>
-          <Toast.Header>
+    <>
+      <Toast onClose={() => setShow(false)} show={show}  className="position-absolute top-0 end-0" animation autohide delay={1000}>
+          <Toast.Header closeButton={false}>
             <strong>Preferance Noted</strong>
             <small> </small>
           </Toast.Header>
-          <Toast.Body>Successfully submitted the status for {date} as {lunchStatus.toUpperCase()}</Toast.Body>
+          <Toast.Body>Successfully submitted the status for {getDate(date)} as {lunchStatus.toUpperCase()}</Toast.Body>
       </Toast>
-
-      <h1 id="name" className="mb-5">
-        Welcome <span className="userName">{employeeData.name}{" "}</span>
-        <span>
-          <Button id="logout" onClick={logout}>
-            Logout
+      <Container className="d-flex flex-column justify-content-center align-items-center vh-100 w-50">
+        <h1 id="name" className="mb-5">
+          Welcome <span className="userName">{employeeData.name}{" "}</span>
+          <span>
+            <Button id="logout" onClick={logout}>
+              Logout
+            </Button>
+          </span>
+        </h1>
+        <Form onSubmit={updateChoice}>
+          <Form.Group className="mb-3" controlId="datePicker">
+            <Form.Label>Select Date</Form.Label>
+            <Form.Control
+              className="datePicker"
+              name="date"
+              type="date"
+              min={new Date().toLocaleString().slice(0, 10).split("/").reverse().join("-")}
+              required
+              value={date.toLocaleDateString().split("/").reverse().join("-")}
+              onChange={(e) => {
+                setDate(new Date(e.target.value));
+              }}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Select Status of your presence: </Form.Label>
+            <Form.Check
+              inline
+              label="Yes"
+              id="yes"
+              name="choice"
+              type="radio"
+              required
+              className="preferanceStatus"
+              onChange={(e) => setLunchStatus("yes")}
+            />
+            <Form.Check
+              inline
+              label="No"
+              id="no"
+              name="choice"
+              type="radio"
+              required
+              className="preferanceStatus mt-3"
+              onChange={(e) => setLunchStatus("no")}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit" className="submitPreferance w-100 mt-4">
+            Submit
           </Button>
-        </span>
-      </h1>
-
-      <Form onSubmit={updateChoice}>
-        <Form.Group className="mb-3" controlId="datePicker">
-          <Form.Label>Select Date</Form.Label>
-          <Form.Control
-            className="datePicker"
-            name="date"
-            type="date"
-            min={new Date().toLocaleString().slice(0, 10).split("/").reverse().join("-")}
-            required
-            value={date.toLocaleDateString().split("/").reverse().join("-")}
-            onChange={(e) => {
-              setDate(new Date(e.target.value));
-            }}
-          />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label>Select Status of your presence: </Form.Label>
-          <Form.Check
-            inline
-            label="Yes"
-            id="yes"
-            name="choice"
-            type="radio"
-            required
-            className="preferanceStatus"
-            onChange={(e) => setLunchStatus("yes")}
-          />
-          <Form.Check
-            inline
-            label="No"
-            id="no"
-            name="choice"
-            type="radio"
-            required
-            className="preferanceStatus mt-3"
-            onChange={(e) => setLunchStatus("no")}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit" className="submitPreferance w-100 mt-4">
-          Submit
-        </Button>
-      </Form>
-    </Container>
+        </Form>
+      </Container>
+    </>
   );
 }
 
